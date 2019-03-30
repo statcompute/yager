2e7219b6bcbcb714aa4c1d8eaaba3d7d1d7f0550
WenSui Liu <liuwensui@gmail.com>
2019-03-30 13:22:23 -0500

    Update GenRandom1D.R

--- code/GenRandom1D.R
+++ code/GenRandom1D.R
@@ -1,16 +1,16 @@
 gen_sobol <- function(min, max, n, seed) {
-  return(round(min + (max - min) * randtoolbox::sobol(n, dim = 1, scrambling = 1, seed = seed), 8))
+  return(round(min + (max - min) * randtoolbox::sobol(n, dim = 1, scrambling = 1, seed = seed), 10))
 }
 
 gen_unifm <- function(min, max, n, seed) {
   set.seed(seed)
-  return(round(min + (max - min) * runif(n), 8))
+  return(round(min + (max - min) * runif(n), 10))
 }
 
 gen_haltn <- function(min, max, n) {
-  return(round(min + (max - min) * randtoolbox::halton(n, dim = 1, usetime = T), 8))
+  return(round(min + (max - min) * randtoolbox::halton(n, dim = 1, init = F), 10))
 }
 
 gen_torus <- function(min, max, n) {
-  return(round(min + (max - min) * randtoolbox::torus(n, dim = 1, usetime = T), 8))
+  return(round(min + (max - min) * randtoolbox::torus(n, dim = 1, init = F), 10))
 }

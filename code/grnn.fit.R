f21b93e3dd1d98576e16243c2c1fdd1ded802545
WenSui Liu <liuwensui@gmail.com>
2019-07-14 14:30:49 -0500

    Update grnn.fit.R

--- code/grnn.fit.R
+++ code/grnn.fit.R
@@ -1,3 +1,14 @@
-grnn.fit <- function(x, y, sigma) {
-  return(grnn::smooth(grnn::learn(data.frame(y, x)), sigma))
+grnn.fit <- function(x, y, sigma = 1) {
+  if (is.matrix(x) == F) stop("x needs to be a matrix.", call. = F)
+  if (anyNA(x) == T) stop("NA found in x.", call. = F)
+  if (is.vector(y) == F) stop("y needs to be a vector.", call. = F)
+  if (anyNA(y) == T) stop("NA found in y.", call. = F)
+  if (length(y) != nrow(x)) stop("x and y need to share the same length.", call. = F)
+  if (sigma <= 0) stop("sigma needs to be positive", call. = F)
+
+  gn <- structure(list(), class = "general regression neural net")
+  gn$x <- x
+  gn$y <- y
+  gn$sigma <- sigma
+  return(gn)
 }

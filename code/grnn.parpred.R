906be75722e87a803caf63bca8bcc7f97a94cc2f
WenSui Liu <liuwensui@gmail.com>
2019-07-17 12:54:36 -0500

    Update grnn.parpred.R

--- code/grnn.parpred.R
+++ code/grnn.parpred.R
@@ -6,7 +6,7 @@ grnn.parpred <- function(net, x) {
   cls <- parallel::makeCluster(min(floor(nrow(x) / 3), parallel::detectCores() - 1), type = "PSOCK")
   parallel::clusterExport(cls, c("net", "grnn.predict", "grnn.predone", "x"), envir = environment())
   spx <- parallel::parLapplyLB(cls, parallel::clusterSplit(cls, seq(nrow(x))),
-                              function(c_) x[c_, ])
+                               function(c_) x[c_, ])
   rst <- parallel::parLapplyLB(cls, spx, function(x_) grnn.predict(net, x_))
   parallel::stopCluster(cls)
   return(Reduce(c, rst))

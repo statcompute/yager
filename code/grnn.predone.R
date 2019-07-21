9885ffabc3e53d445f47ae9263e84c3d55df92d4
WenSui Liu <liuwensui@gmail.com>
2019-07-21 14:43:51 -0500

    Update grnn.predone.R

--- code/grnn.predone.R
+++ code/grnn.predone.R
@@ -1,17 +1,19 @@
 grnn.predone<- function(net, x, type = 1) {
+  ### CHECK INPUT X VECTOR ###
   if (is.vector(x) == F) stop("x needs to be a vector.", call. = F)
   if (anyNA(x) == T) stop("NA found in x.", call. = F)
   if (length(x) != ncol(net$x)) stop("x dimension is not consistent with grnn.", call. = F)
+  ### CHECK INPUT TYPE (CURRENTLY SUPPORTING 1 / 2) ###
   if (!(type %in% c(1, 2))) stop("the type is not supported.", call. = F)
 
   if (type == 1) {
-  ### EUCLIDEAN DISTANCE ###
-    num <- sum(net$y * exp(-Reduce(c, lapply(split(net$x, seq(nrow(net$x))), function(xi) sum((x - xi) ^ 2))) / (2 * (net$sigma ^ 2))))
-    den <- sum(exp(-Reduce(c, lapply(split(net$x, seq(nrow(net$x))), function(xi) sum((x - xi) ^ 2))) / (2 * (net$sigma ^ 2))))
+  ### EUCLIDEAN DISTANCE BY DEFAULT ###
+    num <- sum(net$w * net$y * exp(-Reduce(c, lapply(split(net$x, seq(nrow(net$x))), function(xi) sum((x - xi) ^ 2))) / (2 * (net$sigma ^ 2))))
+    den <- sum(net$w * exp(-Reduce(c, lapply(split(net$x, seq(nrow(net$x))), function(xi) sum((x - xi) ^ 2))) / (2 * (net$sigma ^ 2))))
   } else if (type == 2) {
   ### MANHATTAN DISTANCE ###
-    num <- sum(net$y * exp(-Reduce(c, lapply(split(net$x, seq(nrow(net$x))), function(xi) sum(abs(x - xi)))) / net$sigma))
-    den <- sum(exp(-Reduce(c, lapply(split(net$x, seq(nrow(net$x))), function(xi) sum(abs(x - xi)))) / net$sigma ))
+    num <- sum(net$w * net$y * exp(-Reduce(c, lapply(split(net$x, seq(nrow(net$x))), function(xi) sum(abs(x - xi)))) / net$sigma))
+    den <- sum(net$w * exp(-Reduce(c, lapply(split(net$x, seq(nrow(net$x))), function(xi) sum(abs(x - xi)))) / net$sigma ))
   }
   return(num / den)
 }

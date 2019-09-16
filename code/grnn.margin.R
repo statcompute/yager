516e31ef4b6b9ca2eb6238d797a4c93f8ed9e39b
WenSui Liu <liuwensui@gmail.com>
2019-09-15 19:02:04 -0500

    Update grnn.margin.R

--- code/grnn.margin.R
+++ code/grnn.margin.R
@@ -1,9 +1,8 @@
 grnn.margin <- function(net, n) {
-  x <- matrix(0, nrow = dim(net$x)[1], ncol = dim(net$x)[2])
-  x[, n] <- net$x[, n]
+  x <- matrix(0, nrow = length(unique(net$x[, n])), ncol = dim(net$x)[2])
+  x[, n] <- unique(net$x[, n])
   x.pred <- grnn.parpred(net, x)
   xname <- colnames(net$x)[n]
   plot(sort(x[, n]), x.pred[order(x[, n])], type = "b", lty = 4, lwd = 3, ylab = '', xlab = xname, 
        main = "Marginal Effect", pch = 16, cex = 1.5, col = "red", cex.main = 1, cex.lab = 1, yaxt = 'n')
 }	
-

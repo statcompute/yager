e019ef7c086ef080d3bc54da4700525ae8b82101
WenSui Liu <liuwensui@gmail.com>
2019-09-22 13:15:41 -0500

    Update grnnet.R

--- code/grnnet.R
+++ code/grnnet.R
@@ -112,3 +112,13 @@ grnn.cv_auc <- function(net, sigmas, nfolds, seed = 1) {
   parallel::stopCluster(cls)
   return(list(test = rst, best = rst[rst$auc == max(rst$auc), ]))
 }
+
+grnn.margin <- function(net, i) {
+  n <- length(unique(net$x[, i]))
+  x <- matrix(rep(colMeans(net$x), n), nrow = n, byrow = T)
+  x[, i] <- unique(net$x[, i])
+  x.pred <- grnn.parpred(net, x)
+  xname <- colnames(net$x)[i]
+  plot(sort(x[, i]), x.pred[order(x[, i])], type = "b", lty = 4, lwd = 3, ylab = '', xlab = xname, 
+       main = "Marginal Effect", pch = 16, cex = 1.5, col = "red", cex.main = 1, cex.lab = 1, yaxt = 'n')
+}	                        

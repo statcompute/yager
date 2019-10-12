0cb70970e1c1bd01fae2c269329924d3ee419969
WenSui Liu <liuwensui@gmail.com>
2019-10-12 17:06:39 -0500

    Update and rename grnn.optim_auc.R to grnn.optmiz_auc.R

--- code/grnn.optmiz_auc.R
+++ code/grnn.optmiz_auc.R
@@ -1,9 +1,9 @@
-grnn.optim_auc <- function(net, lower = 0, upper, nfolds = 4, seed = 1, method = 1) {
+grnn.optmiz_auc <- function(net, lower = 0, upper, nfolds = 4, seed = 1, method = 1) {
   if (class(net) != "General Regression Neural Net") stop("net needs to be a GRNN.", call. = F)
   set.seed(seed)
   fd <- caret::createFolds(seq(nrow(net$x)), k = nfolds)
 
-  cv_auc <- function(s) {
+  cv <- function(s) {
     cls <- parallel::makeCluster(min(nfolds, parallel::detectCores() - 1), type = "PSOCK")
     obj <- c("fd", "net", "grnn.fit", "grnn.predone", "grnn.predict")
     parallel::clusterExport(cls, obj,  envir = environment())
@@ -17,10 +17,10 @@ grnn.optim_auc <- function(net, lower = 0, upper, nfolds = 4, seed = 1, method =
   }
 
   if (method == 1) {
-    rst <- optimize(f = cv_auc, interval = c(lower, upper), maximum = T)
+    rst <- optimize(f = cv, interval = c(lower, upper), maximum = T)
   } else if (method == 2) {
-    rst <- optim(par = mean(lower, upper), fn = cv_auc, lower = lower, upper = upper, 
+    rst <- optim(par = mean(lower, upper), fn = cv, lower = lower, upper = upper,
                  method = "Brent", control = list(fnscale = -1))
-  }  
-  return(list(sigma = rst[[1]], auc = rst[[2]]))
+  }
+  return(data.frame(sigma = rst[[1]], auc = rst[[2]]))
 }

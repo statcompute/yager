851a8e0569258a2b375013adac107f53082fa9e1
WenSui Liu <liuwensui@gmail.com>
2019-10-19 08:48:59 -0500

    Update grnn.margin.R

--- code/grnn.margin.R
+++ code/grnn.margin.R
@@ -1,9 +1,14 @@
-grnn.margin <- function(net, i) {
+grnn.margin <- function(net, i, plot = T) {
+  if (class(net) != "General Regression Neural Net") stop("net needs to be a GRNN.", call. = F)
+  xname <- colnames(net$x)[i]
   n <- length(unique(net$x[, i]))
   x <- matrix(rep(colMeans(net$x), n), nrow = n, byrow = T)
-  x[, i] <- unique(net$x[, i])
-  x.pred <- grnn.parpred(net, x)
-  xname <- colnames(net$x)[i]
-  plot(sort(x[, i]), x.pred[order(x[, i])], type = "b", lty = 4, lwd = 3, ylab = '', xlab = xname, 
-       main = "Marginal Effect", pch = 16, cex = 1.5, col = "red", cex.main = 1, cex.lab = 1, yaxt = 'n')
+  x[, i] <- sort(unique(net$x[, i]))
+  rst <- data.frame(x = x[, i], p = grnn.parpred(net, x))
+  if (plot == T) {
+    plot(rst[, 1], rst[, 2], type = "b", lty = 4, lwd = 3, ylab = '', xlab = xname, 
+         main = "Marginal Effect", pch = 16, cex = 1.5, col = "red", cex.main = 1, cex.lab = 1, yaxt = 'n')
+  } else {
+  	return(rst)
+  }
 }	

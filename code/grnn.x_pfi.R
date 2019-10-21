8d07e7d748591175c3d7641d0f7ec32efb082933
WenSui Liu <liuwensui@gmail.com>
2019-10-21 16:40:03 -0500

    Update grnn.x_pfi.R

--- code/grnn.x_pfi.R
+++ code/grnn.x_pfi.R
@@ -1,12 +1,14 @@
-grnn.x_imp <- function(net, i) {
+grnn.x_pfi <- function(net, i, ntry = 1e3, seed = 1) {
   if (class(net) != "General Regression Neural Net") stop("net needs to be a GRNN.", call. = F)
-  if (i > ncol(net$x)) stop("the selected variable is out of bound.", call. = F)
 
   xname <- colnames(net$x)[i]
+  set.seed(seed)
+  seeds <- round(runif(ntry) * 1e8, 0)
+  ol <- lapply(seeds, function(s) with(set.seed(s), sample(seq(nrow(net$x)), nrow(net$x), replace = F)))
+  cl <- Reduce(c, lapply(ol, function(o) abs(cor(seq(nrow(net$x)), o))))
   x <- net$x
-  x[, i] <-  rep(mean(net$x[, i]), length(net$y))
+  x[, i] <-  net$x[ol[[which(cl == min(cl))]], i]
   auc0 <- MLmetrics::AUC(y_pred = grnn.predict(net, net$x), y_true = net$y)
   auc1 <- MLmetrics::AUC(y_pred = grnn.predict(net, x), y_true = net$y)
-  auc2 <- MLmetrics::AUC(y_pred = grnn.predict(grnn.fit(x = x[, -i], y = net$y, sigma = net$sigma), x[, -i]), y_true = net$y)
-  return(data.frame(var = xname, imp1 = round(max(0, 1 - auc1 / auc0), 8), imp2 = round(max(0, 1 - auc2 / auc0), 8)))
+  return(data.frame(var = xname, pfi = round(max(0, 1 - auc1 / auc0), 8)))
 }
